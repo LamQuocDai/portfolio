0029798434df7b8c12e7a1a75cd2f7cb04d8a5f3
Lam Quoc Dai <lamdai210@gmail.com>
2025-02-24 01:01:17 +0700

    first commit

---
+++ data/user-data.js
@@ -0,0 +1,13 @@
+export const userData = {
+  githubUser: 'LamQuocDai',
+  devUsername: "LamQuocDai",
+  github: 'https://github.com/LamQuocDai',
+  facebook: 'https://web.facebook.com/lam.quocdai',
+  linkedIn: 'https://www.linkedin.com/in/%C4%91%E1%BA%A1i-l%C3%A2m-qu%E1%BB%91c-413b9232a/',
+  // twitter: 'https://twitter.com/said7388',
+  // stackOverflow: 'https://stackoverflow.com/users/16840768/abu-said',
+  leetcode: "https://leetcode.com/u/lamquocdai/",
+  resume: "https://drive.google.com/file/d/1KIQokwtHyX-ERDI5ESppuoU0Fza-f5IM/view?usp=sharing",
+  skills: ['React', 'NextJS', 'NodeJS', 'Express', 'Golang', 'Java','Spring boot', 'Hibernate','Python','C#','.Net', 'MySql', 'MongoDB', 'Postgres', 'Docker', 'AWS'],
+  timezone: '+7'
+};
