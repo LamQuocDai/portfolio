6846f2b1685581eb786793aeb79d64e061797990
Lam Quoc Dai <lamdai210@gmail.com>
2025-02-24 02:56:10 +0700

    seventh commit

--- app/components/language/index.jsx
+++ app/components/language/index.jsx
@@ -6,14 +6,17 @@ import SectionTitle from "../helper/section-title";
 
 function GitLanguage() {
   return (
-    <div id="stats" className="relative z-50 border-t my-12 lg:my-24 border-[#25213b]">
+    <div
+      id="stats"
+      className="relative z-50 border-t my-12 lg:my-24 border-[#25213b]"
+    >
       <SectionTitle title="GitHub Languages" />
 
       <div className="py-8 grid grid-cols-1 md:grid-cols-2 gap-5">
         <>
           <GlowCard identifier="repos-per-language">
             <Image
-              src={`http://github-profile-summary-cards.vercel.app/api/cards/repos-per-language?username=${userData.githubUser}&theme=algolia`}
+              src={`https://github-profile-summary-cards.vercel.app/api/cards/repos-per-language?username=${userData.githubUser}&theme=algolia`}
               width={1080}
               height={560}
               alt="github repos-per-language"
@@ -23,7 +26,7 @@ function GitLanguage() {
 
           <GlowCard identifier="most-commit-language">
             <Image
-              src={`http://github-profile-summary-cards.vercel.app/api/cards/most-commit-language?username=${userData.githubUser}&theme=algolia`}
+              src={`https://github-profile-summary-cards.vercel.app/api/cards/most-commit-language?username=${userData.githubUser}&theme=algolia`}
               width={1080}
               height={560}
               alt="github most-commit-language"
@@ -46,6 +49,6 @@ function GitLanguage() {
       </div>
     </div>
   );
-};
+}
 
 export default GitLanguage;
